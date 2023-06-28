41cfa8c97d33ffb1bb6fafca8ca7e17b06f461ea
gcssilva <gustavocssilva1997@gmail.com>
2023-06-28 16:11:25 +0100

    wip

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: gsilva <gsilva@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:52:07 by gsilva            #+#    #+#             */
-/*   Updated: 2023/06/23 18:29:31 by gsilva           ###   ########.fr       */
+/*   Updated: 2023/06/28 15:23:55 by gsilva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	send_sig(int pid, int c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(50);
+		usleep(100);
 	}
 }
 
@@ -41,9 +41,11 @@ int	main(int argc, char **argv)
 {
 	int	pid;
 
-	if (argc != 3)
+	if (argc != 3 || (argc == 3 && !argv[2][0]))
 		return (ft_putstr("Invalid arguments\n"));
 	pid = ft_atoi(argv[1]);
+	if (kill(pid, 0))
+		return (ft_putstr("Invalid PID\n"));
 	while (*argv[2])
 		send_sig(pid, *argv[2]++);
 	send_sig(pid, '\n');
